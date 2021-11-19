3d1f3c2939a172b558061e1db4ba5b35afd27010
frabe1579 <francesco@totalprogrammi.it>
2021-11-19 13:10:24 +0100

    Added extensions with readyHandler to GraphQLHttpClientExtensions

--- src/GraphQL.Client/GraphQLHttpClientExtensions.cs
+++ src/GraphQL.Client/GraphQLHttpClientExtensions.cs
@@ -27,12 +27,41 @@ namespace GraphQL.Client.Http
                     throw e;
             });
 
+        /// <summary>
+        /// Creates a subscription to a GraphQL server. The connection is not established until the first actual subscription is made.<br/>
+        /// All subscriptions made to this stream share the same hot observable.<br/>
+        /// All <see cref="WebSocketException"/>s are passed to the <paramref name="webSocketExceptionHandler"/> to be handled externally.<br/>
+        /// If the <paramref name="webSocketExceptionHandler"/> completes normally, the subscription is recreated with a new connection attempt.<br/>
+        /// Other <see cref="Exception"/>s or any exception thrown by <paramref name="webSocketExceptionHandler"/> will cause the sequence to fail.
+        /// </summary>
+        /// <param name="client">the GraphQL client</param>
+        /// <param name="request">the GraphQL request for this subscription</param>
+        /// <param name="webSocketExceptionHandler">an external handler for all <see cref="WebSocketException"/>s occurring within the sequence</param>
+        /// <param name="readyHandler">Optional: called when the subscription is fully ready to receive messages</param>
+        /// <returns>an observable stream for the specified subscription</returns>
+        public static IObservable<GraphQLResponse<TResponse>> CreateSubscriptionStream<TResponse>(this IGraphQLClient client,
+            GraphQLRequest request, Action<WebSocketException> webSocketExceptionHandler, Action readyHandler) =>
+            client.CreateSubscriptionStream<TResponse>(request, e =>
+            {
+                if (e is WebSocketException webSocketException)
+                    webSocketExceptionHandler(webSocketException);
+                else
+                    throw e;
+            }, readyHandler);
+
         /// <inheritdoc cref="CreateSubscriptionStream{TResponse}(IGraphQLClient,GraphQLRequest,Action{WebSocketException})"/>
         public static IObservable<GraphQLResponse<TResponse>> CreateSubscriptionStream<TResponse>(
             this IGraphQLClient client, GraphQLRequest request, Func<TResponse> defineResponseType, Action<WebSocketException> webSocketExceptionHandler)
         {
             _ = defineResponseType;
             return client.CreateSubscriptionStream<TResponse>(request, webSocketExceptionHandler);
+        }
+
+        public static IObservable<GraphQLResponse<TResponse>> CreateSubscriptionStream<TResponse>(
+            this IGraphQLClient client, GraphQLRequest request, Func<TResponse> defineResponseType, Action<WebSocketException> webSocketExceptionHandler, Action readyHandler)
+        {
+            _ = defineResponseType;
+            return client.CreateSubscriptionStream<TResponse>(request, webSocketExceptionHandler, readyHandler);
         }
 
         /// <inheritdoc cref="GraphQLHttpClient.CreateSubscriptionStream{TResponse}(GraphQLRequest)"/>
